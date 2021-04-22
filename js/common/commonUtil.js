10747971a8cbbe6dab6a9e75b12cd3348d2ed87f
hwlee <hyeongu0305@gmail.com>
2021-04-22 17:55:01 +0900

    날짜가져오기 추가

--- js/common/commonUtil.js
+++ js/common/commonUtil.js
@@ -10,6 +10,10 @@ export default {
     },
     getAllData : () => {
         console.log('getAllData : ');
+    },
+    getYYYYMMDD : (date) => {
+        if ( !date ) date = new Date();
+        return `${date.getFullYear()}${(date.getMonth()+1).length == 1 ? '0'+(date.getMonth()+1) : date.getMonth()+1}${date.getDate()}`
     }
 }
 
