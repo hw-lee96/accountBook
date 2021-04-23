286c4753722337fac9eae1c7088dc368c1b1f2bf
hwlee <hyeongu0305@gmail.com>
2021-04-23 17:52:48 +0900

    버튼 추가

--- pages/Home.js
+++ pages/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
+import dataUtil from '../js/common/dataUtil';
 
 const Home = ({navigation}) => {
     return (
         <View style={st.container}>
             <Text>홈 화면입니다.</Text>
             <Button title="가계부 작성하러 가기" onPress={ () => navigation.push('WriteExpenditure')} />
+            <Text>{"\n"}</Text>
+            <Button title="가계부 조회하러 가기" onPress={ () => navigation.push('ExpenditureList')} />
+            <Text>{"\n"}</Text>
+            <Text>{"\n"}</Text>
+            <Text>{"\n"}</Text>
+            <Button title="가계부 인덱스 초기화" onPress={ () => dataUtil.removeData('expendIdx')} />
+            <Text>{"\n"}</Text>
+            <Button title="가계부 전체 초기화" onPress={ () => dataUtil.removeData('expenditure')} />
+            <Text>{"\n"}</Text>
+            <Button title="전체 데이터 초기화" onPress={ () => dataUtil.dataClearAll('expendIdx')} />
         </View>
     );
 }
