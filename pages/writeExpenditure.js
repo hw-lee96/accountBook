71ab04ede1350f064f800be3fd6e80c28a90bb09
hwlee <hyeongu0305@gmail.com>
2021-04-23 17:54:24 +0900

    스크롤뷰, 가계부 등록 추가

--- pages/writeExpenditure.js
+++ pages/writeExpenditure.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, View, Text, StyleSheet, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Button, View, Text, StyleSheet, TextInput, SafeAreaView, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import cmSt from '../js/common/commonStyleSheet';
 import cm from '../js/common/commonStyleFn';
@@ -46,125 +46,123 @@ const WriteExpenditure = ({ navigation }) => {
     const [category, setCategory] = useState('');
 
     // 등록 버튼
-    const writeSubmit = () => {
+    const writeSubmit = async () => {
         var obj = {
             title,
             content,
             price,
-            spentDate : cmUtil.getYYYYMMDD(date),
+            spentDate: cmUtil.getYYYYMMDD(date),
             category,
-            insDate : cmUtil.getYYYYMMDD(),
-            useYn : 'Y'
+            insDate: cmUtil.getYYYYMMDD(),
+            useYn: 'Y'
         }
-        console.log(obj);
-        dataUtil.setExpend(obj);
+
+        await dataUtil.setExpend(obj);
+
+        Alert.alert( "", "등록되었습니다.", [{ text: "OK", onPress: () => navigation.navigate('Home') }] );
     }
 
-    // 웹 포커스 (이후 모바일에도 적용 예정)
-    
-
     return (
-        <View style={cmSt.FL9, cmSt.dpFlex, cmSt.flAICJCC}>
-            <SafeAreaView style={cmSt.dpFlex, cmSt.flDC, cmSt.flAICJCC, cmSt.btnContainer}>
-                <View style={st.inputBox}>
-                    <View>
-                        <Text style={st.inputTitle}>* 제목</Text>
-                    </View>
-                    <View>
-                        <TextInput style={st.textInput} valut={title} onChangeText={setTitle} placeholder="제목을 입력해주세요" />
-                    </View>
-                </View>
-
-                <View style={st.inputDt}>
-                    <View>
-                        <Text style={st.inputTitle}>* 일자</Text>
-                    </View>
-                    <View>
+        <ScrollView >
+            <View style={cmSt.FL9, cmSt.dpFlex, cmSt.flAICJCC}>
+                <SafeAreaView style={cmSt.dpFlex, cmSt.flDC, cmSt.flAICJCC, cmSt.btnContainer}>
+                    <View style={st.inputBox}>
                         <View>
-                            <Text>{date.getFullYear()}/{(date.getMonth() + 1 < 9 ? '0' : '') + (date.getMonth() + 1)}/{date.getDate()}</Text>
-                            <Button onPress={showDatepicker} title="날짜를 선택해주세요." />
+                            <Text style={st.inputTitle}>* 제목</Text>
                         </View>
-                        {show && (
-                            <DateTimePicker
-                                testID="dateTimePicker"
-                                value={date}
-                                mode={mode}
-                                is24Hour={true}
-                                display="default"
-                                onChange={onChangeDate}
-                            />
-                        )}
+                        <View>
+                            <TextInput style={st.textInput} valut={title} onChangeText={setTitle} placeholder="제목을 입력해주세요" />
+                        </View>
                     </View>
-                </View>
 
-                <View style={st.inputBox}>
-                    <View>
-                        <Text style={st.inputTitle}>* 가격</Text>
+                    <View style={st.inputDt}>
+                        <View>
+                            <Text style={st.inputTitle}>* 일자</Text>
+                        </View>
+                        <View>
+                            <View>
+                                <Text>{date.getFullYear()}/{(date.getMonth() + 1 < 9 ? '0' : '') + (date.getMonth() + 1)}/{date.getDate()}</Text>
+                                <Button onPress={showDatepicker} title="날짜를 선택해주세요." />
+                            </View>
+                            {show && (
+                                <DateTimePicker
+                                    testID="dateTimePicker"
+                                    value={date}
+                                    mode={mode}
+                                    is24Hour={true}
+                                    display="default"
+                                    onChange={onChangeDate}
+                                />
+                            )}
+                        </View>
                     </View>
-                    <View>
-                        <TextInput style={st.textInput} onChangeText={setPrice} placeholder='가격을 입력해주세요'/>
-                    </View>
-                </View>
 
-                <View style={st.inputBox}>
-                    <View>
-                        <Text style={st.inputTitle}>º 내용</Text>
+                    <View style={st.inputBox}>
+                        <View>
+                            <Text style={st.inputTitle}>* 가격</Text>
+                        </View>
+                        <View>
+                            <TextInput style={st.textInput} onChangeText={setPrice} placeholder='가격을 입력해주세요' />
+                        </View>
                     </View>
-                    <View>
-                        <TextInput style={st.textInput} onChangeText={setContent} placeholder='내용을 입력해주세요'/>
-                    </View>
-                </View>
 
-                <View style={st.inputBox}>
-                    <View>
-                        <Text style={st.inputTitle}>º 카테고리</Text>
+                    <View style={st.inputBox}>
+                        <View>
+                            <Text style={st.inputTitle}>º 내용</Text>
+                        </View>
+                        <View>
+                            <TextInput style={st.textInput} onChangeText={setContent} placeholder='내용을 입력해주세요' />
+                        </View>
                     </View>
-                    <View>
-                        <TextInput style={st.textInput} onChangeText={setCategory} placeholder='카테고리를 입력해주세요'/>
-                    </View>
-                </View>
-            </SafeAreaView>
 
-            <View style={cmSt.botBtn}>
-                {/* <Text>가계부 작성</Text> */}
-                <TouchableOpacity style={cmSt.submitBtn} onPress={writeSubmit} >
-                    <View>
-                        <Text style={cmSt.submitText}>등록</Text>
+                    <View style={st.inputBox}>
+                        <View>
+                            <Text style={st.inputTitle}>º 카테고리</Text>
+                        </View>
+                        <View>
+                            <TextInput style={st.textInput} onChangeText={setCategory} placeholder='카테고리를 입력해주세요' />
+                        </View>
                     </View>
-                </TouchableOpacity>
-            </View>
-                {/* <View style={cmSt.submitBtn} ><Button title="등록" onPress={writeSubmit} /></View> */}
+                </SafeAreaView>
+
+                <View style={cmSt.botBtn}>
+                    {/* <Text>가계부 작성</Text> */}
+                    <TouchableOpacity style={cmSt.submitBtn} onPress={writeSubmit} >
+                        <View>
+                            <Text style={cmSt.submitText}>등록</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
                 <Text>{"\n"}</Text>
                 {/* <br> 대신 {"\n"} 사용, 문자로 취급하므로 <Text>로 감싸야됨*/}
-                {/* <Button style={cmSt.submitBtn} title="홈 화면으로 가기" onPress={() => navigation.navigate('Home')} /> */}
-        </View>
+            </View>
+        </ScrollView>
     )
 }
 
 const st = StyleSheet.create({
-    inputBox : {
-        width : cm.dw(80),
-        height : cm.dh(5),
-        marginTop : cm.dh(4),
-        marginBottom : cm.dh(4)
+    inputBox: {
+        width: cm.dw(80),
+        height: cm.dh(5),
+        marginTop: cm.dh(4),
+        marginBottom: cm.dh(4)
     },
-    inputDt : {
-        width : cm.dw(80),
-        height : cm.dh(10),
-        marginTop : cm.dh(4),
-        marginBottom : cm.dh(4)
+    inputDt: {
+        width: cm.dw(80),
+        height: cm.dh(10),
+        marginTop: cm.dh(4),
+        marginBottom: cm.dh(4)
     },
-    inputTitle : {
-        fontSize : 16,
-        fontWeight : '600',
-        marginBottom : cm.dh(1),
+    inputTitle: {
+        fontSize: 16,
+        fontWeight: '600',
+        marginBottom: cm.dh(1),
     },
-    textInput : {
-        margin : 5,
-        borderBottomColor : 'black',
-        backgroundColor : '#fff'
+    textInput: {
+        margin: 5,
+        borderBottomColor: 'black',
+        backgroundColor: '#fff'
     }
-
 });
 
 export default WriteExpenditure;
