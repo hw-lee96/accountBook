fdfed779c012fcba78007b913057ded6d782630a
hwlee <hyeongu0305@gmail.com>
2021-04-30 16:47:36 +0900

    미사용 함수 삭제

--- js/common/commonUtil.js
+++ js/common/commonUtil.js
@@ -2,15 +2,6 @@
 import { Dimensions } from 'react-native';
 
 export default {
-    setData : () => {
-        console.log('setData : ');
-    },
-    getData : () => {
-        console.log('getData : ');
-    },
-    getAllData : () => {
-        console.log('getAllData : ');
-    },
     getYYYYMMDD : (date) => {
         if ( !date ) date = new Date();
         return `${date.getFullYear()}${(date.getMonth()+1).length == 1 ? '0'+(date.getMonth()+1) : date.getMonth()+1}${date.getDate()}`
